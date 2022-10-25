e5c3e9751a8d4a9a2757c83486807e5df8e92d3d
Dominique Foley <dfoleydev@gmail.com>
2022-10-25 01:30:34 -0400

    Installed jwt and bcrypt for user auth. Create userModel, userController, userRoute. Need to add auth and start work on frontend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+// @desc Register new user
+// @route POST /api/users
+//access Public
+const registerUser = (req, res) => {
+    res.json({ message: 'Register User' })
+}
+
+// @desc Authenticate a user
+// @route POST /api/users/login
+//access Public
+const loginUser = (req, res) => {
+    res.json({ message: 'Login User' })
+}
+
+// @desc Get user data
+// @route GET /api/users/me
+//access Public
+const getMe = (req, res) => {
+    res.json({ message: 'User data display' })
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
