99f85b1552f5e5b95fa4594556d0e4285149c54a
Dominique Foley <dfoleydev@gmail.com>
2022-11-01 00:55:12 -0400

    Setup frontend register add loading spinner

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            //Only really need token
             token: generateToken(user._id),
         })
     } else {
