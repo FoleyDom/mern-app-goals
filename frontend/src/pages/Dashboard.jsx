e398a035126474f3dc9ceb0adc2c2a3211e419bb
Dominique Foley <dfoleydev@gmail.com>
2023-01-28 12:29:33 -0500

    Browser hanging fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,54 +7,50 @@ import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const { user } = useSelector((state) => state.auth)
-    const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
+  const { user } = useSelector((state) => state.auth)
+  const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
 
-    useEffect(() => {
-        if (isError) {
-            console.log(message)
-        }
-
-        if (!user) {
-            navigate('/login')
-        }
-
-        dispatch(getGoals())
-
-        return () => {
-            dispatch(reset())
-        }
-    }, [user, navigate, isError, message, dispatch])
-
-    if (isLoading) {
-        return <Spinner />
+  useEffect(() => {
+    if (isError) {
+      console.log(message)
     }
 
-    return (
-        <>
-            <section className="heading">
-                <h1>Welcome {user && user.name}</h1>
-                <p>Goals Dashboard</p>
-            </section>
+    if (!user) {
+      navigate('/login')
+    } else {
+      dispatch(getGoals())
+    }
+  }, [user, navigate, isError, message, dispatch])
 
-            <GoalForm />
+  if (isLoading) {
+    return <Spinner />
+  }
 
-            <section className="content">
-                {goals.length > 0 ? (
-                    <div className="goals">
-                        {goals.map((goal) => (
-                            <GoalItem key={goal._id} goal={goal} />
-                        ))}
-                    </div>
-                ) : (
-                    <h3>You have not set any goals</h3>
-                )}
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section>
+    </>
+  )
 }
 
 export default Dashboard
