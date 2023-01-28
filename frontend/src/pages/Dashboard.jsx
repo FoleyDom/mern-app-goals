c23eb4d75b015e69eb95a7c9d7c8a5e1ccaa887b
Dominique Foley <dfoleydev@gmail.com>
2023-01-28 12:31:41 -0500

    Browser hanging fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import { getGoals } from '../features/goals/goalSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
